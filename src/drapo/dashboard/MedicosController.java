fa7c5b4aab853348d4fb779539ce251614cb819b
henriqueup <titepessoa@gmail.com>
2018-12-03 18:51:36 -0200

    tratamento de erros no cadastro do medico e classe com infos do medico

--- src/drapo/dashboard/MedicosController.java
+++ src/drapo/dashboard/MedicosController.java
@@ -24,9 +24,9 @@ import javafx.scene.input.*;
 public class MedicosController implements Initializable {    
     
     private int cbSelected = 0;
-    List<String> medicosNomes = new ArrayList<String>();
     List<String> medicosEspecialidades = new ArrayList<String>();
     String horas[] = { "07:00-13:00", "08:00-14:00", "09:00-15:00", "10:00-16:00", "11:00-17:00", "12:00-18:00" };
+    List<Medico> medicos = new ArrayList<Medico>();
     
     @FXML private TextField textfield_nome;
     
@@ -54,10 +54,77 @@ public class MedicosController implements Initializable {
         }
     }
     
+    private boolean entradaOK(int tempo, int horario, boolean dias[], String nome, List<String> especialidades){
+        if(tempo == -1)
+            return false;
+        
+        if(horario == -1)
+            return false;
+        
+        int cntDias=0;
+        for(boolean dia : dias)
+            if(dia)
+                cntDias++;
+        if(cntDias != 3)
+            return false;
+        
+        if(Objects.equals("", nome))
+            return false;
+        
+        if(especialidades.isEmpty())
+            return false;
+        
+        return true;
+    }
+    
     @FXML
-    private void botao_salvar(ActionEvent event) {  
-//        medicosNomes.add(textfield_nome.getText());
-        System.out.println(textfield_nome.getText());
+    private void botao_salvar(ActionEvent event) {
+        int tempo=-1, horario=-1, i;
+        boolean dias[] = new boolean[7];
+        String nome = "";
+        List<String> especialidades = new ArrayList<String>();
+        
+        if(rb_15.isSelected())
+            tempo = 15;
+        else if(rb_20.isSelected())
+            tempo = 20;
+        else if(rb_30.isSelected())
+            tempo = 30;
+        
+        for(i=0;i<6;i++)
+            if(cb_hora.getValue() == horas[i]){
+                horario = i;
+                break;
+            }
+        
+        if(cb_seg.isSelected())
+            dias[0] = true;
+        if(cb_ter.isSelected())
+            dias[1] = true;
+        if(cb_qua.isSelected())
+            dias[2] = true;
+        if(cb_qui.isSelected())
+            dias[3] = true;
+        if(cb_sex.isSelected())
+            dias[4] = true;
+        if(cb_sab.isSelected())
+            dias[5] = true;
+        if(cb_dom.isSelected())
+            dias[6] = true;
+        
+        nome = textfield_nome.getText();
+        
+        ObservableList<String> items = cb_especialidades.getItems();
+        for(String item : items){
+            especialidades.add(item);
+        }
+        
+        if(entradaOK(tempo, horario, dias, nome, especialidades)){
+            Medico novo = new Medico(tempo, horario, dias, nome, especialidades);
+            System.out.println(novo.toString());
+        }else{
+            System.out.println("DEU MERDA");
+        }
     }
     
     @FXML
